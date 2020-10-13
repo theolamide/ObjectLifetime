9ec55a346011a5ce94c43311fd9c95de56514c08
theolamide <ajadidan@gmail.com>
2020-10-13 17:09:06 -0400

    Understading of static methods and contructors

--- ObjectLifetime/Program.cs
+++ ObjectLifetime/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ObjectLifetime
 {
@@ -6,7 +7,57 @@ namespace ObjectLifetime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car myCar = new Car();
+
+            //Example of using overloaded constructor
+            Car myThirdCar = new Car("Ford", "Explorer", 2020, "Blue");
+
+            myCar.Make = "OldsMobile";
+            myCar.Model = "Cutlass Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+
+            Car myOtherCar;
+            myOtherCar = myCar; //They are both pointed to the same bucket in memory here. If you rename with with tag, tehy both change.
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+
+            myOtherCar.Model = "98";
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        //Contructor
+        public Car()
+        {
+            Make = "Nissan"; //This automatically sets every new instance of car make to Nissan without doing so manually.
+        }
+
+        //Overloaded contructor
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
         }
     }
 }
